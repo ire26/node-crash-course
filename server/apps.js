27aadb6e5ee56cd563f2e090b645837f27ad3142
Ireee <oyetiboire@gmail.com>
2024-11-01 09:44:19 +0100

    new commit

--- server/apps.js
+++ server/apps.js
@@ -3,25 +3,38 @@ const express = require('express') //this invokes the express function
 // express app
 const app = express()
 
+//register view engine
+app.set('view engine', 'ejs')
+
 //listen for requests 
 app.listen(3000)
 
-app.get('/', (req, res) =>{    //it takes in 2 arguments: what aprt of the url you want to listen to
+app.get('/', (req, res) =>{    //it takes in 2 arguments: what part of the url you want to listen to
     //res.send('<p>Home Page</p>')
-    res.sendFile('./views/index.html', {root: __dirname})
+    const blogs = [
+        {title: 'Ire wakes up', snippet: 'orem ipsum dolor sit amet consectetur'},
+        {title: 'Ire presses his phone', snippet: 'orem ipsum dolor sit amet consectetur'},
+        {title: 'Ire writes some codes', snippet: 'orem ipsum dolor sit amet consectetur'}
+    ]
+    res.render('index', {title:'Home', blogs:blogs})
 })
 
 app.get('/about', (req, res) =>{    
     //res.send('<p>About Page</p>')
-    res.sendFile('./views/about.html', {root: __dirname})
+    res.render('about', {title:'About'})
 })
 
 //REDIRECTS
-app.get('/about-me', (req,res) =>{
-    res.redirect('/about')
+//app.get('/about-me', (req,res) =>{
+//    res.redirect('/about')
+//})
+
+
+app.get('/blogs/create', (req,res) =>{
+    res.render('create', {title:'Create'})
 })
 
 // 404 page
 app.use((req, res) =>{
-    res.sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404', {title:'404'})
 })
